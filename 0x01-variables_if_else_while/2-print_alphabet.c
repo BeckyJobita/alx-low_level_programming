ddddb90b70cf048d162d080a1bf95bc4875a5500
BeckyJobita <joyolonga@gmail.com>
2023-02-17 20:54:04 +0300

    Prints out letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Return: Always (0) is successful
+ */
+int main(void)
+{
+int ltr;
+for (ltr = 'a'; ltr <= 'z'; ltr++)
+putchar(ltr);
+putchar('\n');
+return (0);
+}
