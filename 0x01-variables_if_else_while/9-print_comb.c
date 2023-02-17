21096fb917c2fea8eaaa39af2122ba5e246e90e0
BeckyJobita <joyolonga@gmail.com>
2023-02-17 23:50:56 +0300

    Print out 10 digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+int d1;
+for (d1 = 48; d1 < 58; d1++)
+{
+putchar(d1);
+if (d1 != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
