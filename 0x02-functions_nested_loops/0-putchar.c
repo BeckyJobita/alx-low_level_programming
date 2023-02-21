f9eacc436c3f6208ae9fb23e41eab2b3409a0eb8
BeckyJobita <joyolonga@gmail.com>
2023-02-21 14:39:33 +0300

    program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * main -Entry
+ * Return: 0
+ */
+int _putchar(char);
+
+int main(void)
+{
+	char *ch = "_putchar";
+	while (*ch)
+	{
+	_putchar(*ch);
+	ch++;
+	}
+	_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#0x02. C - Functions, nested loops
+File 0 has a program that prints _putchar, followed by a new line.
