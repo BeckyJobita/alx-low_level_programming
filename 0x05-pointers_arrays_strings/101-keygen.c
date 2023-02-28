1c7ebcacaae4cf28988b52c6c3c7f7ee49e755d7
BeckyJobita <joyolonga@gmail.com>
2023-02-28 10:02:17 -0800

    program that generates random valid passwords for the program

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,19 +12,19 @@ int main(void)
 
 	srand(time(0));
 
-	while (sum < 27772)
+	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
 	}
 	password[index] = '\0';
 
-	if (sum != 27772)
+	if (sum != 2772)
 	{
-		diff_half1 = (sum - 27772) / 2;
-		diff_half2 = (sum - 27772) / 2;
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
 
-		if ((sum - 27772) % 2 != 0)
+		if ((sum - 2772) % 2 != 0)
 		{
 			diff_half1++;
 
@@ -40,11 +40,13 @@ int main(void)
 			for (index = 0; password[index]; index++)
 			{
 				if (password[index] >= (33 + diff_half2))
-
-				break;
+				{
+					password[index] -= diff_half2;
+					break;
 			}
 		}
 	}
 	printf("%s", password);
 	return (0);
+	}
 }
